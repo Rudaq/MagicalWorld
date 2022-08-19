da6642660923bf73677fc32a4561731acada56e4
DESKTOP-L4DHRG1\wekrz <we.krzeminska@gmail.com>
2022-08-19 09:12:27 +0200

    little code restructuring and button change

---
+++ game/Button.py
@@ -0,0 +1,34 @@
+import pygame
+
+
+# button class
+class Button(pygame.sprite.Sprite):
+    def __init__(self, x, y, image, scale):
+        super().__init__()
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+
+    def draw(self):
+        action = False
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        return action
+
+    def change_image(self, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
